228951f87217f10712dc5d36d510de3fbcfc3428
Alexey404Ivanov <AA.Ivanov2@urfu.me>
2024-05-08 17:47:54 +0500

    Add coin counter

---
+++ Assets/Scripts/CoinText.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CoinText : MonoBehaviour
+{
+    private Text CoinTexts;
+    public static int Coin;
+
+    private void Start()
+    {
+        CoinTexts = GetComponent<Text>();
+        Coin = PlayerPrefs.GetInt("Coins", Coin);
+    }
+
+    private void Update()
+    {
+        CoinTexts.text = Coin.ToString();
+    }
+}
