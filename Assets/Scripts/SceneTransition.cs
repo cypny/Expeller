118397738be7d5a95c9f0355a72429c3ce7cb7d9
Alexey404Ivanov <AA.Ivanov2@urfu.me>
2024-05-10 01:01:38 +0500

    Make UI

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -6,8 +6,26 @@ using UnityEngine.SceneManagement;
 public class SceneTransition : MonoBehaviour
 {
     public int sceneNumber;
+    public Canvas canvas;
+    public Canvas canvas1;
+    public Canvas canvas2;
+    public Canvas storeCanvas;
+    
     public void Transition()
     {
-        SceneManager.LoadScene(sceneNumber);
+        if (sceneNumber == 1)
+        {
+            canvas.enabled = false;
+            canvas1.enabled = false;
+            canvas2.enabled = false;
+            storeCanvas.enabled = true;
+        }
+        else
+        {
+            canvas.enabled = true;
+            canvas1.enabled = true;
+            canvas2.enabled = true;
+            storeCanvas.enabled = false;
+        }
     }
 }
